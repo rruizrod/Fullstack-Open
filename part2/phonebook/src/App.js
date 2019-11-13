bd4a079fa891c853d1b539eaebf51082fc4a6ff5
Ricardo Ruiz <kxngvenom@gmail.com>
2019-11-13 00:03:37 -0500

    Part 2b PhoneBook 2.4 Working

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleNameChange = event => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = event => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearch = event => {
+    console.log(event.target.value);
+    setSearch(event.target.value);
+  };
+
+  const handleNameAdd = event => {
+    event.preventDefault();
+    let x = persons.map(person => person.name);
+    x = x.indexOf(newName);
+    if (x >= 0) {
+      window.alert(newName + " is already added to your phonebook");
+    } else {
+      console.log("Button Clicked", event.target);
+      const personOBJ = {
+        name: newName,
+        number: newNumber
+      };
+      setPersons(persons.concat(personOBJ));
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const personsList = () => {
+    const personsFilter = persons.filter(person =>
+      person.name.includes(search)
+    );
+    const people = () =>
+      personsFilter.map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ));
+    return <div>{people()}</div>;
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      filter shown with
+      <input value={search} onChange={handleSearch} />
+      <h3>Add A New</h3>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleNameAdd}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {personsList()}
+    </div>
+  );
+};
+
+export default App;
