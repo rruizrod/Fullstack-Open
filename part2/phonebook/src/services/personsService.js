8c11bc1d4dc8b12c1f6837b2349298e9eb9d1906
Ricardo Ruiz <kxngvenom@gmail.com>
2019-11-14 22:31:05 -0500

    Part2d 2.17 done

---
+++ part2/phonebook/src/services/personsService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3001/persons";
+
+const getAll = () => {
+  const request = axios.get(baseURL);
+  return request.then(response => response.data);
+};
+
+const add = newPerson => {
+  const request = axios.post(baseURL, newPerson);
+  return request.then(response => response.data);
+};
+
+const del = id => {
+  const request = axios.delete(`${baseURL}/${id}`, id);
+  return request.then(response => response.data);
+};
+
+export default { getAll, add, del };
