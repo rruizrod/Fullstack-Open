27923f8a5b24239b03fcc0a72adda3583a88847b
Ricardo Ruiz <kxngvenom@gmail.com>
2019-11-15 23:13:58 -0500

    3b done

---
+++ part3/phonebookbackend/build/precache-manifest.2e0fa26fce435eb90438096507e804d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e40d3831f6555e1f051e58fbb74d79f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49534a17eb26ead96821",
+    "url": "/static/css/main.12648587.chunk.css"
+  },
+  {
+    "revision": "0705d1fea8e49a586295",
+    "url": "/static/js/2.d0857bc7.chunk.js"
+  },
+  {
+    "revision": "49534a17eb26ead96821",
+    "url": "/static/js/main.3607227e.chunk.js"
+  },
+  {
+    "revision": "df99deec2933845bfe71",
+    "url": "/static/js/runtime-main.b4276010.js"
+  }
+]);
