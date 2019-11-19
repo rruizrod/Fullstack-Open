4141be0f6d2ba8a767d7f6329df1248793e547fa
Ricardo Ruiz <kxngvenom@gmail.com>
2019-11-19 11:45:29 -0500

    Part4 started 4.2 refactoring complete and working

--- part3/phonebookbackend/models/person.js
+++ part3/phonebookbackend/models/person.js
@@ -1,19 +1,5 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-mongoose.set("useFindAndModify", false);
-mongoose.set("useUnifiedTopology", true);
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url, { useNewUrlParser: true })
-  .then(result => {
-    console.log("Connected to DB");
-  })
-  .catch(error => {
-    console.log("Error connecting to MongoDB:", error);
-  });
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, required: true, unique: true },
---
+++ part4/bloglist/app.js
@@ -0,0 +1,17 @@
+const config = require("./utils/config");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogs");
+const mongoose = require("mongoose");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
